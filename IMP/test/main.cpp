3f5a5ea2dfce7c726a9b07a4a3ffdefb2ec38036
ftxj <cs.xinjie@gmail.com>
2020-08-29 16:50:46 +0800

    test add constant

--- IMP/test/main.cpp
+++ IMP/test/main.cpp
@@ -1,10 +1,18 @@
-#include "tensorflow.hpp"
-
+#include "tfAPI/tensorflow.hpp"
+#include "data_structure/ndarray.hpp"
+#include <string>
 int main() {
     TensorFlow tf;
-    auto x = tf.constant<int>(3, , Shape(1));
-    auto y = tf.constant<int>(4, , Shape(1));
-    auto A = tf.math.add(x, y);
+    auto x = tf.constant(3, "int32", Shape(1), "x");
+    auto y = tf.constant(4, "int32", Shape(1), "y");
+    auto A = tf.math.add(x, y, "add");
+
+    auto x2 = tf.constant(3, "int32", Shape(1), "x2");
+    auto y2 = tf.constant(4, "int32", Shape(1), "y2");
+    auto A2 = tf.math.add(x2, y2, "add2");
+    
+    auto A3 = tf.math.add(A, A2, "add3");
+
     tf.run();
     return 0;
 }
---
+++ IMP/test/run.sh
@@ -0,0 +1,9 @@
+cd ..
+make clean
+wait
+make
+wait
+cd test
+make clean
+make
+./main.test
